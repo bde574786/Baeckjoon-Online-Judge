5d965f68f72e8a1243da6f598c43375bcd476fb3
bde574786 <102010541+bde574786@users.noreply.github.com>
2024-07-29 10:36:09 +0900

    [Silver IV] Title: 큐, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,32 @@
+import sys
+
+n = int(sys.stdin.readline())
+queue = []
+
+for i in range(n):
+    command = sys.stdin.readline().split()
+    
+    if command[0] == 'push':
+        queue.append(command[1])
+    elif command[0] == 'pop':
+        if not queue:
+            print(-1)
+        else:
+            print(queue.pop(0))
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == 'front':
+        if not queue:
+            print(-1)
+        else:
+            print(queue[0])
+    elif command[0] == 'back':
+        if not queue:
+            print(-1)
+        else:
+            print(queue[-1])
