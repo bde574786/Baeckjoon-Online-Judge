73a1c80cb2ba308dd4d899c120c82bd07d738e3a
bde574786 <102010541+bde574786@users.noreply.github.com>
2024-08-05 11:49:58 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -0,0 +1,19 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    clothes = {}
+    
+    for _ in range(n):
+        name, kind = input().split()
+        if kind in clothes:
+            clothes[kind] += 1
+        else:
+            clothes[kind] = 1
+    
+    result = 1
+    for count in clothes.values():
+        result *= count + 1
+        
+    result -= 1
+    print(result)
