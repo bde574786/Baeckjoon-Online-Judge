147f70e553d8513dd7ec41cd6953737a49681e1b
bde574786 <102010541+bde574786@users.noreply.github.com>
2024-09-03 19:28:18 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,10 @@
+import sys
+
+words = [sys.stdin.readline().strip() for _ in range(5)]
+result = []
+for i in range(15):
+    for word in words:
+        if i < len(word):
+            result.append(word[i])
+            
+print("".join(result))
