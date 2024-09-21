1ab59cf0dc76b45f8d96ae3f8ab7d3cd85bd45c8
bde574786 <102010541+bde574786@users.noreply.github.com>
2024-09-21 15:27:08 +0900

    [Bronze III] Title: 네 번째 점, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,24 @@
+points = []
+for _ in range(3):
+    x, y = map(int, input().split())
+    points.append((x, y))
+
+x1, y1 = points[0]
+x2, y2 = points[1]
+x3, y3 = points[2]
+
+if x1 == x2:
+    x4 = x3
+elif x1 == x3:
+    x4 = x2
+else:
+    x4 = x1
+
+if y1 == y2:
+    y4 = y3
+elif y1 == y3:
+    y4 = y2
+else:
+    y4 = y1
+
+print(x4, y4, sep=' ')
