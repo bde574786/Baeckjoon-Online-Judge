4f959e7ce393b56b71b7ab78c795eddf5e8617eb
bde574786 <102010541+bde574786@users.noreply.github.com>
2024-09-29 00:39:56 +0900

    [Silver IV] Title: 문자열 집합, Time: 544 ms, Memory: 36564 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+
+s = set()
+for _ in range(n):
+    s.add(input().strip())
+
+count = 0
+for _ in range(m):
+    query = input().strip()
+    if query in s:
+        count += 1
+
+print(count)
